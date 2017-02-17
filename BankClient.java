00fe517b3eb9b2e2f436264f7ae03254aa4eb7bf
nitilaksha h <nitilaksha@nitilakshas-MacBook-Pro.local>
2017-02-16 21:24:36 -0600

    Updated Client code and modularized the code a bit

--- BankClient.java
+++ BankClient.java
@@ -6,23 +6,99 @@ import responsetypes.*;
 
 class BankClient {
 	
+	private ArrayList<Integer> accountlist;
+
+	ArrayList<Integer> getAccountList () {
+		return accountlist;
+	}
+
+	void newAccountRequest (ObjectOutputStream os, ObjectInputStream oin, PrintWriter writer) throws IOException{
+
+		NewAccountRequest accreq = new NewAccountRequest("NewAccountRequest");
+		os.writeObject(accreq);
+		try {
+			Response resp = (Response)oin.readObject();
+			NewAccountCreationResponse accresp = (NewAccountCreationResponse)resp;
+			writer.println("Request Name: "+ accresp.getReqName());
+			writer.println("Returned UID: "+ accresp.getResponse());
+			writer.println();	
+			accountlist.add(accresp.getResponse());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void depositRequest (ObjectOutputStream os, ObjectInputStream oin, PrintWriter writer, int uID, int amount) throws IOException{
+		
+		DepositRequest depreq = new DepositRequest("DepositRequest", uID, amount);
+		os.writeObject(depreq);
+
+		try {
+			Response resp = (Response)oin.readObject();
+			DepositResponse depresp = (DepositResponse)resp;
+			writer.println("Request Name: "+ depresp.getReqName());
+			writer.println("Returned Status: "+ depresp.getResponse());	
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void balanceRequest (ObjectOutputStream os, ObjectInputStream oin, PrintWriter writer, int uID) throws IOException{
+		
+		BalanceRequest balreq = new BalanceRequest("BalanceRequest", uID);
+		os.writeObject(balreq);
+
+		try {
+			Response resp = (Response)oin.readObject();
+			BalanceResponse balresp = (BalanceResponse)resp;
+			writer.println("Request Name: "+ balresp.getReqName());
+			writer.println("Returned Balance: "+ balresp.getResponse());	
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void processTask () {
+
+	}
+
 	public static void main (String [] args) {
 
 		String hostname = args[0];
 		int portNumber = Integer.parseInt(args[1]);
-		String userInput;
+		int threadCount = Integer.parseInt(args[2]);
+		int iterationCount = Integer.parseInt(args[3]);
+		BankClient bankclient = new BankClient();
 
 		try(Socket echoSocket = new Socket (hostname, portNumber);
 			OutputStream out = echoSocket.getOutputStream();
-			BufferedReader in = new BufferedReader (new InputStreamReader(echoSocket.getInputStream()));
+			InputStream in = echoSocket.getInputStream();
 			BufferedReader stdIn = new BufferedReader (new InputStreamReader (System.in))) {
-			
+
 			ObjectOutputStream os = new ObjectOutputStream(out);
-			
 			os.writeObject(new Request("NewAccountCreation"));
-			//os.writeObject( "Now I understand this example of object serialization.");
-			//System.out.println (in.readLine());	
+			ObjectInputStream oin = new ObjectInputStream(in);
+
+			PrintWriter writer = new PrintWriter("clientLog.txt", "UTF-8");	
+
+			//Creating 100 accounts on the server
+			for (int i = 0; i < 100; i++)
+				bankclient.newAccountRequest (os, oin, writer);
 			
+			//Depositing 100 in each of those accounts
+			for (int i = 0; i < 100; i++) {
+				ArrayList<Integer> list = bankclient.getAccountList();
+				int accid = list.get(i);
+				bankclient.depositRequest(os, oin, writer, accid, 100);
+			}
+
+			writer.close();
 
 		}catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostname);
@@ -31,6 +107,6 @@ class BankClient {
 			System.err.println("Couldn't get I/O for the connection to " +
                 hostname);
             System.exit(1);
-		}
+		} 
 	}
 }
