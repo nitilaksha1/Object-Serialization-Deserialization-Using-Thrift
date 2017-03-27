37cce0c27a3508164ce8717d1c08d7204b5f8672
mulka002 <mulka002@umn.edu>
2017-03-27 16:42:10 -0500

    Update BankService.thrift

--- Apache Thrift/BankService.thrift
+++ Apache Thrift/BankService.thrift
@@ -1,33 +1,3 @@
-public class Account {
-	int UID;
-	int balance;
-
-	public Account (int UID, int balance) {
-		this.UID = UID;
-		this.balance = balance;
-	}
-
-	public int getUID () {
-		return UID;
-	}
-
-	public void deposit (int amount) {
-		balance += amount;
-	}
-
-	public void withdraw (int amount) {
-
-		if ((balance - amount) < 0)
-			return;
-
-		balance -= amount;
-	}
-
-	public int getBalance () {
-		return balance;
-	}
-
-}
 
 service BankService {
 
